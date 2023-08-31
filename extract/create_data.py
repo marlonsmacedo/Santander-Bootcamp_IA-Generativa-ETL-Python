d1bf9802e75c78ada9f77fdfc4296e3eed4081fb
Mmarlon Macedo <marlon.s.macedo@gmail.com>
2023-08-31 19:15:50 -0300

    feat: alter create data comments

--- extract/create_data.py
+++ extract/create_data.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-Create users data to https://sdw-2023-prd.up.railway.app/users/ and get IDs from the new users created.
+# Create users data to https://sdw-2023-prd.up.railway.app/users/ and get IDs from the new users created.
